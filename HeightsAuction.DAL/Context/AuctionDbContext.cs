8a459747a2463797209266a78e127fb8656058a9
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-06-22 23:40:00 +0100

    Added DbContext

---
+++ HeightsAuction.DAL/Context/AuctionDbContext.cs
@@ -0,0 +1,22 @@
+﻿using HeightsAuction.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeightsAuction.DAL.Context
+{
+    
+    public class AuctionDbContext : DbContext
+    {
+        public AuctionDbContext(DbContextOptions<AuctionDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Bid> Bids { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+    }
+
+}
