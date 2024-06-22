d408e16b6b5610e944083e3a1f40890bc0377bfc
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-06-22 23:42:42 +0100

    Added Entities

---
+++ HeightsAuction.DAL/Entities/Bid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeightsAuction.DAL.Entities
+{
+    public class Bid
+    {
+        public int Id { get; set; }
+        public int RoomId { get; set; }
+        public int UserId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ HeightsAuction.DAL/Entities/Invoice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeightsAuction.DAL.Entities
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int RoomId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
