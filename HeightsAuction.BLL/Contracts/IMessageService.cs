2a068833305ba145db2f279124179b986294d26c
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-06-24 23:58:52 +0100

    Added new interface

---
+++ HeightsAuction.BLL/Contracts/IMessageService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeightsAuction.BLL.Contracts
+{
+    public interface IMessageService
+    {
+        Task SendMessage(string user, string message);
+
+    }
+}
