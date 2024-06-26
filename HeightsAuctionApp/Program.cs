432b7bb11be0b78afb40f6f47df2351181f8a9ba
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-06-26 13:40:12 +0100

    Updated the program file and registered new services

--- HeightsAuctionApp/Program.cs
+++ HeightsAuctionApp/Program.cs
@@ -1,3 +1,8 @@
+using HeightsAuction.BLL;
+using HeightsAuction.BLL.Contracts;
+using HeightsAuction.BLL.Services;
+using HeightsAuction.DAL.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace HeightsAuctionApp
 {
@@ -8,10 +13,23 @@ namespace HeightsAuctionApp
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<AuctionDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConn")));
 
+            //builder.Services.AddAutoMapper(typeof(Program));
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+            builder.Services.AddTransient<IUserService, UserService>();
+            builder.Services.AddTransient<IRoomService, RoomService>();
+            builder.Services.AddTransient<IBidService, BidService>();
+            builder.Services.AddTransient<IInvoiceService, InvoiceService>();
+            builder.Services.AddTransient<IPaymentService, PaymentService>();
+            builder.Services.AddTransient<IMessageService, MessageService>();
+            builder.Services.AddTransient<INotificationService, NotificationService>();
+
+            builder.Services.AddSignalR();
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
@@ -19,16 +37,24 @@ namespace HeightsAuctionApp
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            app.UseRouting();
 
             app.UseAuthorization();
 
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<AuctionHub>("/auctionHub");
+            });
 
-            app.MapControllers();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "AuctionChatApp V1");
+            });
 
             app.Run();
         }
