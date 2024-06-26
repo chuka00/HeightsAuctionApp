cb3e3086cd7a81de50440951d6ba72d414e01636
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-06-26 13:32:40 +0100

    Added BidController

---
+++ HeightsAuctionApp/Controllers/BidController.cs
@@ -0,0 +1,33 @@
+﻿using HeightsAuction.BLL.Contracts;
+using HeightsAuction.DAL.Mapping.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeightsAuctionApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BidController : ControllerBase
+    {
+        private readonly IBidService _bidService;
+
+        public BidController(IBidService bidService)
+        {
+            _bidService = bidService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SubmitBid([FromBody] BidDto bidDto)
+        {
+            var createdBid = await _bidService.SubmitBid(bidDto);
+            return Ok(createdBid);
+        }
+
+        [HttpGet("{roomId}")]
+        public async Task<IActionResult> GetBids(int roomId)
+        {
+            var bids = await _bidService.GetBids(roomId);
+            return Ok(bids);
+        }
+    }
+}
